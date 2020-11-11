5d757fe4cc1c6be20eb406f53ad83b80e187f6b2
RESOL <52667827+CashingBOT@users.noreply.github.com>
2020-11-11 18:34:59 +0800

    Add files via upload

---
+++ TumblePanda/src/settings.598ca.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scene/main.fire",orientation:"",jsList:["assets/script/Game/Lib/Core.min.5a4b7.js"],bundleVers:{internal:"45e63",resources:"4d87a",main:"75b1a"}};
